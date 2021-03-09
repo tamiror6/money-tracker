b262fa3c78584ee65978f184c2f6c1c6643dd957
tamiro <tamiro@wix.com>
2021-03-09 14:57:08 +0200

    statring general flow

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen ,fireEvent} from '@testing-library/react';
 import App from './App';
+
 
 describe("check good flow of the app",()=>{
   beforeEach(()=>{
     render(<App />);
   })
-  test('renders title', () => {
-    expect(screen.queryByTestId("app-title")).not.toBeNull()
-    
+  test('general flow of app', () => {
+    expect(screen.queryByTestId("app-title")?.innerHTML).toBe("Money Tracker")
+    expect(screen.queryByTestId("total-expenses")?.innerHTML.includes("Total: 0")).toBe(true)
+    const amountInput = screen.getByTestId("amount")
+    fireEvent.change(amountInput, { target: { value: "51" } })
   });
 })
--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,7 +5,9 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h2 data-testid="app-title"></h2>
+      <h2 data-testid="app-title">Money Tracker</h2>
+      <h2 data-testid="total-expenses">Total: 0</h2>
+      <input data-testid="amount"></input>
     </div>
   );
 }
